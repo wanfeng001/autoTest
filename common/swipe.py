73804deae909c659825781ef4a2dd923c223c2b3
浩涛1 <376531877@qq.com>
2020-12-16 15:47:03 +0800

    滑动封装

---
+++ common/swipe.py
@@ -0,0 +1,58 @@
+from time import sleep
+
+from appium import webdriver
+des ={
+'deviceName':'M2004J7AC',
+'platformName':'Android',
+'platformVersion':'10',
+'appPackage':'com.taobao.taobao',
+'appActivity':'com.taobao.tao.welcome.Welcome',
+'resetKeyboard':'true'
+}
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',des)
+
+def swipeUp(driver,t=500,n=1):
+    l = driver.get_window_size()
+    x1= l['width']* 0.5
+    y1= l['height']* 0.25
+    y2= l['height']* 0.75
+    for i in range(n):
+        driver.swipe(x1,y1,x1,y2,t)
+
+def swipeDown(driver,t=500,n=1):
+    l = driver.get_window_size()
+    x1= l['width']* 0.5
+    y1= l['height']* 0.75
+    y2= l['height']* 0.25
+    for i in range(n):
+        driver.swipe(x1,y1,x1,y2,t)
+
+def swipeRight(driver,t=500,n=1):
+    l = driver.get_window_size()
+    x1= l['width']* 0.5
+    x2= l['width']* 0.75
+    y1= l['height']* 0.5
+    for i in range(n):
+        driver.swipe(x1,y1,x2,y1,t)
+
+def swipeLeft(driver,t=500,n=1):
+    l = driver.get_window_size()
+    x1= l['width']* 0.5
+    x2= l['width']* 0.25
+    y1= l['height']* 0.5
+    for i in range(n):
+        driver.swipe(x1,y1,x2,y1,t)
+
+if __name__ == "__main__":
+    ac = driver.current_activity
+    driver.wait_activity(ac, 20)
+    driver.find_element_by_id('com.taobao.taobao:id/provision_positive_button').click()
+    driver.find_element_by_id('com.lbe.security.miui:id/permission_allow_foreground_only_button').click()
+    sleep(3)
+    swipeDown(driver,n=2)
+    sleep(3)
+    swipeRight(driver,n=2)
+    sleep(3)
+    swipeLeft(driver,n=2)
+    driver.close_app()
+
--- script/app.py
+++ script/app.py
@@ -1,11 +1,23 @@
 # coding=utf-8
 from appium import webdriver
+
+from common import swipe
+
 des ={
-'deviceName':'127.0.0.1:62001',
+'deviceName':'M2004J7AC',
 'platformName':'Android',
-'platformVersion':'7.0.0.6',
-'appPackage':'com.qzd.harddisk',
-'appActivity':'com.qzd.harddisk.activity.SplashScreenActivity'
+'platformVersion':'10',
+'appPackage':'com.taobao.taobao',
+'appActivity':'com.taobao.tao.welcome.Welcome',
+'resetKeyboard':'true'
 }
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',des)
 
+ac = driver.current_activity
+print (ac)
+
+driver.wait_activity(ac,10)
+driver.find_element_by_id('com.taobao.taobao:id/provision_positive_button').click()
+driver.find_element_by_id('com.lbe.security.miui:id/permission_allow_foreground_only_button').click()
+swipe.swipeDown(driver,n=3)
+driver.close_app()

62b2f702c0b151d11490b5ab07c65e13de7ff668
浩涛1 <376531877@qq.com>
2020-12-16 17:32:25 +0800

    轮播图滑动

--- script/app.py
+++ script/app.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from time import sleep
+
 from appium import webdriver
 
 from common import swipe
@@ -15,9 +17,19 @@ driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',des)
 
 ac = driver.current_activity
 print (ac)
-
 driver.wait_activity(ac,10)
 driver.find_element_by_id('com.taobao.taobao:id/provision_positive_button').click()
+ac1 = driver.current_activity
+sleep(2)
 driver.find_element_by_id('com.lbe.security.miui:id/permission_allow_foreground_only_button').click()
-swipe.swipeDown(driver,n=3)
+# swipe.swipeDown(driver,n=3)
+ac2 = driver.current_activity
+driver.wait_activity(ac2,20)
+l = driver.get_window_size()
+x1=l['width']*0.8
+y1=l['height']*0.1
+x2=l['width']*0.2
+for i in range(2):
+    driver.swipe(x1,y1,x2,y1,500)
+sleep(10)
 driver.close_app()

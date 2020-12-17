2dc685601af16bcc8560c49b6b80aa9b64b355f7
浩涛1 <376531877@qq.com>
2020-12-17 16:17:46 +0800

    封装

--- app.py
+++ app.py
@@ -5,19 +5,36 @@ from logging import handlers
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print (BASE_DIR)
+
+'''
+    Logger 记录器，暴露了应用程序代码能直接使用的接口。
+    Handler 处理器，将（记录器产生的）日志记录发送至合适的目的地。
+    Filter 过滤器，提供了更好的粒度控制，它可以决定输出哪些日志记录。
+    Formatter 格式化器，指明了最终输出中日志记录的布局。
+'''
+
 def init_logging():
+    # 初始化记录器
     logger = logging.getLogger()
+    # 设置日志等级
     logger.setLevel(logging.INFO)
+    # 设置日志的格式
     fmt = ('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
     formatter = logging.Formatter(fmt)
+    # 将日志输除到屏幕上的处理器 sh
     sh = logging.StreamHandler()
+    # 添加日志格式
     sh.setFormatter(formatter)
-    log_path = BASE_DIR +"/logs/test.log" # 日志存放位置
+    # 日志存放位置
+    log_path = BASE_DIR +"/logs/test.log"
+    # 将日志输出到指定位置的处理器 th
     th = handlers.TimedRotatingFileHandler(filename=log_path,
                                            interval=1,
                                            backupCount=3,
                                            when='S',
                                            encoding='utf-8')
+    # 添加日志格式
     th.setFormatter(formatter)
+    # 添加处理器
     logger.addHandler(sh)
     logger.addHandler(th)
--- script/app.py
+++ script/app.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 from time import sleep
-
+from selenium import webdriver
 from appium import webdriver
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from common import swipe
 
 des ={
@@ -11,7 +13,9 @@ des ={
 'platformVersion':'10',
 'appPackage':'com.taobao.taobao',
 'appActivity':'com.taobao.tao.welcome.Welcome',
-'resetKeyboard':'true'
+'resetKeyboard':'true',
+'unicodeKeyboard':'true',
+'automationName':'Uiautomator2'
 }
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',des)
 
@@ -20,11 +24,11 @@ print (ac)
 driver.wait_activity(ac,10)
 driver.find_element_by_id('com.taobao.taobao:id/provision_positive_button').click()
 ac1 = driver.current_activity
-sleep(2)
+sleep(5)
 driver.find_element_by_id('com.lbe.security.miui:id/permission_allow_foreground_only_button').click()
 # swipe.swipeDown(driver,n=3)
-ac2 = driver.current_activity
-driver.wait_activity(ac2,20)
+'''
+滑动
 l = driver.get_window_size()
 x1=l['width']*0.8
 y1=l['height']*0.1
@@ -32,4 +36,77 @@ x2=l['width']*0.2
 for i in range(2):
     driver.swipe(x1,y1,x2,y1,500)
 sleep(10)
+'''
+
+'''
+定位toast
+driver.press_keycode(4)
+toast_loc= ('xpath','.//*[contains(@text,“再按一次返回键退出手机淘宝”)]')
+t = WebDriverWait(driver,20,0.1).until(EC.presence_of_element_located(toast_loc))
+print (t)
+'''
+'''
+# 封装toast是否存在
+driver.press_keycode(4)
+def is_toast_exist(driver,text,timeout=20,throttle=0.1):
+    try:
+        loc = ('xpath', './/*[contains(@text,{})]'.format(text))
+        print(loc)
+        WebDriverWait(driver,timeout,throttle).until(EC.presence_of_element_located(loc))
+        return True
+    except:
+        return False
+a = is_toast_exist(driver,"再按一次返回键就退出手机淘宝")
+print (a)
+'''
+
+# 点击第一张图片
+'''
+ac2 =driver.current_activity
+print (ac2)
+driver.wait_activity(ac2,20)
+try:
+    driver.implicitly_wait(10)
+    ele = driver.find_elements_by_class_name("android.widget.ImageView")[1].click()
+    print (ele)
+    sleep(3)
+except Exception as e:
+    print(e)
+'''
+
+ac = driver.current_activity
+print (ac)
+driver.wait_activity(ac,10)
+driver.tap([(431,140),(200,140)],500)
+
+def search_button(driver):
+    ele = driver.find_element_by_accessibility_id('搜索')
+    return  ele
+
+def search_kuang(driver):
+    ele = driver.find_element_by_id('com.taobao.taobao:id/searchEdit')
+    return  ele
+
+def search_kuanghou(driver):
+    ele = driver.find_element_by_id('com.taobao.taobao:id/search_bar_wrapper')
+    return ele
+
+# 封装元素等待
+def wait_element(driver,locator,timeout=20):
+    try:
+        ele = WebDriverWait(driver,timeout,0.1).until(EC.presence_of_element_located(locator))
+        return ele
+    except Exception as e:
+        driver.save_screenshot(r'C:\Users\1111111\Desktop\app')
+        print (e)
+
+search_kuang(driver).send_keys('测试')
+search_button(driver).click()
+search_kuanghou(driver).click()
+loc =(By.ID,'com.taobao.taobao:id/searchEdit')
+wait_element(driver,loc).send_keys('啊啊啊')
+search_button(driver).click()
+sleep(5)
+
 driver.close_app()
+

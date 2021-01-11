ba51e408cccb3b0050089becad0dff3d59265443
haotao <376531877@qq.com>
2021-01-11 18:55:58 +0800

    1

--- common/readConfig.py
+++ common/readConfig.py
@@ -1,23 +1,23 @@
 import os
 import configparser
-# print (config_file)
-# class Readconfig():
-#     def __init__(self):
-#         self.config_path = os.path.join(os.getcwd(), "..\data\cfg.ini")
-#         self.config = configparser.ConfigParser()
-#         self.config.read(self.config_path,encoding='utf-8')
-#
-#     # QQ邮箱配置
-#     @classmethod
-#     def qq_ini(cls,value):
-#         value = Readconfig().config.get('email_qq',value)
-#         return value
-#
-#     # Mysql配置
-#     @classmethod
-#     def my_ini(cls,value):
-#         value =Readconfig().config.get('mysql_db',value)
-#         return value
+
+class Readconfig():
+    def __init__(self):
+        self.config_path = os.path.join(os.getcwd(), "..\data\cfg.ini")
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_path,encoding='utf-8')
+
+    # QQ邮箱配置
+    @classmethod
+    def qq_ini(cls,value):
+        value = Readconfig().config.get('email_qq',value)
+        return value
+
+    # Mysql配置
+    @classmethod
+    def my_ini(cls,value):
+        value =Readconfig().config.get('mysql_db',value)
+        return value
 
 # print(Readconfig.qq_ini('email_host'))
 

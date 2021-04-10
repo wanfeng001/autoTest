27e4170818144dc182bc572268579158ad867e55
haotao <376531877@qq.com>
2021-04-10 15:25:15 +0800

    Commit

--- run_suite.py
+++ run_suite.py
@@ -9,9 +9,6 @@ class RunCase(unittest.TestCase):
     file_path = configpath.report_path
     case_path = configpath.case_path
     with open(file_path,mode='wb') as f:
-        #suite = unittest.TestSuite()
-        #suite.addTest(unittest.TestLoader().loadTestsFromTestCase(Test_001))
-        #suite = unittest.makeSuite(Test_001)
         suite = unittest.defaultTestLoader.discover(case_path,pattern='testcase*',top_level_dir=None)
         HTMLTestRunner.HTMLTestRunner(stream=f,verbosity=2,title='测试报告',description='测试结果').run(suite)
     SendEmail().send_email(file_path)

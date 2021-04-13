e23d8df422627768f223346537fa5a3cb541e82f
wanfeng001 <376531877@qq.com>
2021-04-13 19:37:42 +0800

    update

--- api/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-from tools.setLog import init_logging
-import logging
-init_logging() # 初始化日志
-logging.info('ceshi666')
--- script/test_login.py
+++ script/test_login.py
@@ -4,8 +4,9 @@ from common.operateExcel import OperateExcel
 import pytest
 import allure
 from selenium.webdriver.common.by import By
+
 # 初始化日志
-logger =Logger().getlog()
+logger = Logger().getlog()
 # 账号元素
 account = (By.XPATH, '//*[@id="u"]')
 # 密码元素
@@ -22,28 +23,23 @@ name = (By.XPATH, '//*[@id="useralias"]')
 hua_button = (By.XPATH, '//*[@id="tcaptcha_drag_thumb"]')
 
 # 测试用例名称
-case1 = OperateExcel().get_test_case(sheet_name='登录模块用例',id='DL-DLYM-001')
-case2 = OperateExcel().get_test_case(sheet_name='登录模块用例',id='DL-DLYM-002')
-case3 = OperateExcel().get_test_case(sheet_name='登录模块用例',id='DL-DLYM-003')
-case4 = OperateExcel().get_test_case(sheet_name='登录模块用例',id='DL-DLYM-004')
-case5 = OperateExcel().get_test_case(sheet_name='登录模块用例',id='DL-DLYM-005')
+case1 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-001')
+case2 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-002')
+case3 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-003')
+case4 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-004')
+case5 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-005')
 # 账号密码
-case1Account = OperateExcel().get_account_data(sheet_name='登录模块用例',id='DL-DLYM-001')
-case2Account = OperateExcel().get_account_data(sheet_name='登录模块用例',id='DL-DLYM-002')
-case3Account = OperateExcel().get_account_data(sheet_name='登录模块用例',id='DL-DLYM-003')
-case4Account = OperateExcel().get_account_data(sheet_name='登录模块用例',id='DL-DLYM-004')
-case5Account = OperateExcel().get_account_data(sheet_name='登录模块用例',id='DL-DLYM-005')
+case1Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-001')
+case2Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-002')
+case3Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-003')
+case4Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-004')
+case5Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-005')
 # 是否执行
-isCase1Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例',id='DL-DLYM-001')
-isCase2Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例',id='DL-DLYM-002')
-isCase3Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例',id='DL-DLYM-003')
-isCase4Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例',id='DL-DLYM-004')
-isCase5Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例',id='DL-DLYM-005')
-
-
-
-
-
+isCase1Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-001')
+isCase2Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-002')
+isCase3Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-003')
+isCase4Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-004')
+isCase5Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-005')
 
 
 @allure.feature('登录功能')
@@ -52,7 +48,7 @@ class Test_login():
     if isCase1Execute:
         @allure.severity(severity_level='normal')
         @allure.story('用例1：{}'.format(case1))
-        @allure.title('# 登录场景测试')
+        @allure.title('#{}'.format(case1))
         def test_correct_login(self, browser):
             '''正确账号、密码'''
             browser.switch_to.frame(id)
@@ -69,119 +65,125 @@ class Test_login():
                 bt.click()
             with allure.step("校验结果"):
                 try:
-                    err = browser.find_element(error_msg[0], error_msg[1]).text
-                    logger.info(err)
                     time.sleep(1)
                     t1 = browser.title
                     assert "QQ邮箱" == t1, '没有登录进去'
-                    OperateExcel().write_result(sheet_name='登录模块用例',id='DL-DLYM-001',value='pass')
+                    OperateExcel().write_result(sheet_name='登录模块用例', id='DL-DLYM-001', value='pass')
+                    logger.info('登录成功')
                 except Exception as e:
                     logger.info(e)
     else:
         logger.info('此用例IsExecute:{},不执行！'.format(isCase1Execute))
 
-    @allure.story('用例2：错误的账号')
-    @allure.title('# 错误的账号')
-    @pytest.mark.skip
-    def test_error_account(self, browser):
-        '''错误的账号'''
-        browser.switch_to.frame(id)
-        ac = browser.find_element(account[0], account[1])
-        ac.clear()
-        ac.send_keys(1)
-        pw = browser.find_element(password[0], password[1])
-        pw.clear()
-        pw.send_keys()
-        bt = browser.find_element(button[0], button[1])
-        bt.click()
-        time.sleep(3)
-        try:
-            err = browser.find_element(error_msg[0], error_msg[1]).text
-            logger.info(err)
-        except Exception as e:
-            logger.info(e)
+    if isCase1Execute:
+        @allure.story('用例2：{}'.format(case2))
+        @allure.title('#{}'.format(case2))
+        # @pytest.mark.skip
+        def test_error_account(self, browser):
+            '''错误的账号'''
+            browser.switch_to.frame(id)
+            ac = browser.find_element(account[0], account[1])
+            ac.clear()
+            ac.send_keys(1)
+            pw = browser.find_element(password[0], password[1])
+            pw.clear()
+            pw.send_keys()
+            bt = browser.find_element(button[0], button[1])
+            bt.click()
+            time.sleep(3)
+            try:
+                err = browser.find_element(error_msg[0], error_msg[1]).text
+                logger.info(err)
+                time.sleep(1)
+                t1 = browser.title
+                assert "QQ邮箱" == t1, '没有登录进去'
+            except Exception as e:
+                OperateExcel().write_result(sheet_name='登录模块用例', id='DL-DLYM-002', value='fail')
+                logger.info(e)
+    else:
+        logger.info('此用例IsExecute:{},不执行！'.format(isCase1Execute))
 
-    @allure.story('用例3：错误的密码')
-    @allure.title('# 错误的密码')
-    @pytest.mark.skip
-    def test_error_password(self, browser):
-        '''错误的密码'''
-        browser.switch_to.frame(id)
-        ac = browser.find_element(account[0], account[1])
-        ac.clear()
-        ac.send_keys()
-        pw = browser.find_element(password[0], password[1])
-        pw.clear()
-        pw.send_keys(123456)
-        bt = browser.find_element(button[0], button[1])
-        bt.click()
-        time.sleep(3)
-        try:
-            err = browser.find_element(error_msg[0], error_msg[1]).text
-            logger.info(err)
-        except Exception as e:
-            logger.info(e)
+    if isCase1Execute:
+        @allure.story('用例3：{}'.format(case3))
+        @allure.title('#{}'.format(case3))
+        # @pytest.mark.skip
+        def test_error_password(self, browser):
+            '''错误的密码'''
+            browser.switch_to.frame(id)
+            ac = browser.find_element(account[0], account[1])
+            ac.clear()
+            ac.send_keys()
+            pw = browser.find_element(password[0], password[1])
+            pw.clear()
+            pw.send_keys(123456)
+            bt = browser.find_element(button[0], button[1])
+            bt.click()
+            time.sleep(3)
+            try:
+                err = browser.find_element(error_msg[0], error_msg[1]).text
+                logger.info(err)
+                time.sleep(1)
+                t1 = browser.title
+                assert "QQ邮箱" == t1, '没有登录进去'
+            except Exception as e:
+                OperateExcel().write_result(sheet_name='登录模块用例', id='DL-DLYM-003', value='fail')
+                logger.info(e)
+    else:
+        logger.info('此用例IsExecute:{},不执行！'.format(isCase1Execute))
 
-    @allure.story('用例4：账号为空')
-    @allure.title('# 账号为空')
-    @pytest.mark.skip
-    def test_null_account(self, browser):
-        '''账号为空'''
-        browser.switch_to.frame(id)
-        ac = browser.find_element(account[0], account[1])
-        ac.clear()
-        ac.send_keys()
-        pw = browser.find_element(password[0], password[1])
-        pw.clear()
-        pw.send_keys()
-        bt = browser.find_element(button[0], button[1])
-        bt.click()
-        try:
-            err = browser.find_element(error_msg[0], error_msg[1]).text
-            logger.info(err)
-        except Exception as e:
-            logger.info(e)
+    if isCase1Execute:
+        @allure.story('用例4：{}'.format(case4))
+        @allure.title('#{}'.format(case4))
+        # @pytest.mark.skip
+        def test_null_account(self, browser):
+            '''账号为空'''
+            browser.switch_to.frame(id)
+            ac = browser.find_element(account[0], account[1])
+            ac.clear()
+            ac.send_keys()
+            pw = browser.find_element(password[0], password[1])
+            pw.clear()
+            pw.send_keys()
+            bt = browser.find_element(button[0], button[1])
+            bt.click()
+            try:
+                err = browser.find_element(error_msg[0], error_msg[1]).text
+                logger.info(err)
+                time.sleep(1)
+                t1 = browser.title
+                assert "QQ邮箱" == t1, '没有登录进去'
+            except Exception as e:
+                OperateExcel().write_result(sheet_name='登录模块用例', id='DL-DLYM-004', value='fail')
+                logger.info(e)
+    else:
+        logger.info('此用例IsExecute:{},不执行！'.format(isCase1Execute))
 
-    @allure.story('用例5：密码为空')
-    @allure.title('# 密码为空')
-    @pytest.mark.skip
-    def test_null_password(self, browser):
-        '''密码为空'''
-        browser.switch_to.frame(id)
-        ac = browser.find_element(account[0], account[1])
-        ac.clear()
-        ac.send_keys()
-        pw = browser.find_element(password[0], password[1])
-        pw.clear()
-        pw.send_keys()
-        bt = browser.find_element(button[0], button[1])
-        bt.click()
-        try:
-            err = browser.find_element(error_msg[0], error_msg[1]).text
-            logger.info(err)
-        except Exception as e:
-            logger.info(e)
-
-    @allure.story('用例6：失败用例截图')
-    @allure.title('# 用例执行失败')
-    @allure.severity(severity_level='blocker')
-    @pytest.mark.skip
-    def test_worry_serict(self, browser):
-        '''密码为空'''
-        browser.switch_to.frame(id)
-        ac = browser.find_element(account[0], account[1])
-        ac.clear()
-        ac.send_keys()
-        pw = browser.find_element(password[0], password[1])
-        pw.clear()
-        pw.send_keys()
-        bt = browser.find_element(button[0], button[2])
-        bt.click()
-        try:
-            err = browser.find_element(error_msg[0], error_msg[1]).text
-            logger.info(err)
-        except Exception as e:
-            logger.info(e)
+    if isCase1Execute:
+        @allure.story('用例5：{}'.format(case5))
+        @allure.title('#{}'.format(case5))
+        # @pytest.mark.skip
+        def test_null_password(self, browser):
+            '''密码为空'''
+            browser.switch_to.frame(id)
+            ac = browser.find_element(account[0], account[1])
+            ac.clear()
+            ac.send_keys()
+            pw = browser.find_element(password[0], password[1])
+            pw.clear()
+            pw.send_keys()
+            bt = browser.find_element(button[0], button[1])
+            bt.click()
+            try:
+                err = browser.find_element(error_msg[0], error_msg[1]).text
+                logger.info(err)
+                time.sleep(1)
+                t1 = browser.title
+                assert "QQ邮箱" == t1, '没有登录进去'
+            except Exception as e:
+                OperateExcel().write_result(sheet_name='登录模块用例', id='DL-DLYM-005', value='fail')
+                logger.info(e)
+    else:
+        logger.info('此用例IsExecute:{},不执行！'.format(isCase1Execute))
 
 
 if __name__ == '__main__':

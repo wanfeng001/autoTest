6886c7095a3f7f7fa9e3ad649d561f0c83af9e4f
wanfeng001 <376531877@qq.com>
2021-04-14 09:58:39 +0800

    Commit

--- page/loginPage.py
+++ page/loginPage.py
@@ -1,7 +1,40 @@
 # -*- coding: utf-8 -*-
-
-'''
+"""
 @Time    : 2021/4/14 8:50
 @Author  : wanfeng
+"""
 
-'''
+from selenium.webdriver.common.by import By
+from common.operateExcel import OperateExcel
+
+# 账号元素
+account = (By.XPATH, '//*[@id="u"]')
+# 密码元素
+password = (By.XPATH, '//*[@id="p"]')
+# 登录按钮
+button = (By.XPATH, '//*[@id="login_button"]')
+# 错误信息提示
+error_msg = (By.XPATH, '//*[@id="err_m"]')
+# iframe
+id = 'login_frame'
+# 断言值:名字
+name = (By.XPATH, '//*[@id="useralias"]')
+
+# 测试用例名称
+case1 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-001')
+case2 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-002')
+case3 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-003')
+case4 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-004')
+case5 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-005')
+# 账号密码
+case1Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-001')
+case2Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-002')
+case3Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-003')
+case4Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-004')
+case5Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-005')
+# 是否执行
+isCase1Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-001')
+isCase2Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-002')
+isCase3Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-003')
+isCase4Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-004')
+isCase5Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-005')
--- script/test_login.py
+++ script/test_login.py
@@ -4,43 +4,9 @@ from common.operateExcel import OperateExcel
 import pytest
 import allure
 from selenium.webdriver.common.by import By
-
+from page.loginPage import *
 # 初始化日志
 logger = Logger().getlog()
-# 账号元素
-account = (By.XPATH, '//*[@id="u"]')
-# 密码元素
-password = (By.XPATH, '//*[@id="p"]')
-# 登录按钮
-button = (By.XPATH, '//*[@id="login_button"]')
-# 错误信息提示
-error_msg = (By.XPATH, '//*[@id="err_m"]')
-# iframe
-id = 'login_frame'
-# 断言值:名字
-name = (By.XPATH, '//*[@id="useralias"]')
-# 滑动按钮 22.5px 193.5px  移动距离 171px
-hua_button = (By.XPATH, '//*[@id="tcaptcha_drag_thumb"]')
-
-# 测试用例名称
-case1 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-001')
-case2 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-002')
-case3 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-003')
-case4 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-004')
-case5 = OperateExcel().get_test_case(sheet_name='登录模块用例', id='DL-DLYM-005')
-# 账号密码
-case1Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-001')
-case2Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-002')
-case3Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-003')
-case4Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-004')
-case5Account = OperateExcel().get_account_data(sheet_name='登录模块用例', id='DL-DLYM-005')
-# 是否执行
-isCase1Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-001')
-isCase2Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-002')
-isCase3Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-003')
-isCase4Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-004')
-isCase5Execute = OperateExcel().get_is_execute(sheet_name='登录模块用例', id='DL-DLYM-005')
-
 
 @allure.feature('登录功能')
 @allure.testcase('https://mail.qq.com/', 'qq邮箱')
@@ -72,6 +38,7 @@ class Test_login():
                     logger.info('登录成功')
                 except Exception as e:
                     logger.info(e)
+                    OperateExcel().write_result(sheet_name='登录模块用例', id='DL-DLYM-001', value='fail')
     else:
         logger.info('此用例IsExecute:{},不执行！'.format(isCase1Execute))
 

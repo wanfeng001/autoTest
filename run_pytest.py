1ae045d3e4c2a6e9d995b7406a078a278c613a17
wanfeng001 <376531877@qq.com>
2021-04-15 17:53:11 +0800

    Commit

--- run_pytest.py
+++ run_pytest.py
@@ -8,7 +8,7 @@ import os
 import pytest
 
 # pytest程序调用
-pytest.main(['--alluredir=report/allure-raw','--clean-alluredir'])
+pytest.main(['-n=3','--alluredir=report/allure-raw','--clean-alluredir'])
 # 执行CMD命令
 cmd ='allure generate report/allure-raw -o report/allure-report --clean'
 os.system(cmd)
